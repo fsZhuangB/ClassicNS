9b263e6ab9f6e7bb618aac63b839d8746baf811c
Brendon J. Brewer <brendon.brewer@gmail.com>
2022-01-20 08:38:54 +1300

    Comput elog z

--- SqliteDB.cpp
+++ SqliteDB.cpp
@@ -1,4 +1,7 @@
 #include "SqliteDB.h"
+#include "Tools/Misc.hpp"
+
+using Tools::logsumexp;
 
 namespace ClassicNS
 {
@@ -19,8 +22,9 @@ void SqliteDB::create_tables()
     db << "DROP TABLE IF EXISTS particles;";
 
     db << "CREATE TABLE IF NOT EXISTS sampler_info\
-            (rng_seed INTEGER NOT NULL PRIMARY KEY)\
-            WITHOUT ROWID;";
+            (rng_seed      INTEGER NOT NULL,\
+             num_particles INTEGER NOT NULL,\
+             mcmc_steps    INTEGER NOT NULL);";
 
     db << "CREATE TABLE IF NOT EXISTS particles\
             (iteration      INTEGER NOT NULL PRIMARY KEY,\
@@ -34,6 +38,25 @@ void SqliteDB::create_tables()
 sqlite::database& SqliteDB::get_db()
 {
     return db;
+}
+
+double SqliteDB::compute_ln_z()
+{
+    double log_numerator   = -1E300;
+    double log_denominator = -1E300;
+
+    int num_particles;
+    db << "SELECT num_particles FROM sampler_info;" >> num_particles;
+
+    db << "SELECT iteration, log_likelihood FROM particles;" >>
+        [&](int iteration, double log_likelihood)
+        {
+            log_numerator = logsumexp({log_numerator,
+                             -(double)iteration/num_particles + log_likelihood});
+            log_denominator = logsumexp({log_denominator,
+                                 -(double)iteration/num_particles});
+        };
+    return log_numerator - log_denominator;
 }
 
 } // namespace
