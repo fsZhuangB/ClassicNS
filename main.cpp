bf583d4cb4a364a43cbd19d1baaec8e8b6739d01
fszhuangb <fszhuangb@qq.com>
2022-02-15 12:08:48 +0800

    add comments

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ using Model = SpikeSlab;
 
 int main()
 {
+    // initial the sampler
+    // model is from the spike and slab
     Sampler<Model> sampler(123);
     sampler.run_to_depth(100.0);
     return 0;
